db39ac4f2f8f4ee643a474b635ef3699d4b1e266
Nagashima Sayuri <106061832+ngsm-syr@users.noreply.github.com>
2025-05-30 09:53:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//javascript
+
+// body の背景色を変える
+const btnSample01 = document.querySelector('.sample01');
+btnSample01.addEventListener('click', () => {
+  const obj = document.body;
+  obj.style.backgroundColor = 'gray';
+});
+
+// ID要素 の背景色を変える
+const btnSample02 = document.querySelector('.sample02');
+btnSample02.addEventListener('click', () => {
+  const obj = document.getElementById('id01');
+  obj.style.backgroundColor = 'orange';
+});
+
+// CLASS要素 の背景色を変える
+const btnSample03 = document.querySelector('.sample03');
+btnSample03.addEventListener('click', () => {
+  const lines = document.getElementsByClassName('class01');
+  for (let i = 0; i < lines.length; i++) {
+    lines[i].style.backgroundColor = 'green';
+  }
+});
+
+// HTML要素 の背景色を変える
+const btnSample04 = document.querySelector('.sample04');
+btnSample04.addEventListener('click', () => {
+  const obj = document.getElementsByTagName('p');
+  for (let i = 0; i < obj.length; i++) {
+    obj[i].style.backgroundColor = 'blue';
+  }
+});
